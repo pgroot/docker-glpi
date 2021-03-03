5364bdf7c1ba9173ad50193025ab8598e32cf69f
GROOT <groot.pan@goldendata.cloud>
2021-03-03 11:38:08 +0800

    plugins ...

---
+++ plugins/fields/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.12.2',
+    'version' => '1.12.2.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd118fd8017cd1920e5fcaa7eb46c8093a3c069e7',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.12.2',
+      'version' => '1.12.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd118fd8017cd1920e5fcaa7eb46c8093a3c069e7',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v4.4.19',
+      'version' => '4.4.19.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '17ed9f14c1aa05b1a5cf2e2c5ef2d0be28058ef9',
+    ),
+  ),
+);
